b93c3cc0496a4499f942a97fab5841b13e906c8e
Nyoni <tafnyoni123@gmail.com>
2021-06-17 11:03:19 +0200

    game window updated

--- Arcade Game/arcade_game/02_game_window.py
+++ Arcade Game/arcade_game/02_game_window.py
@@ -1,5 +1,5 @@
 import arcade 
-import pathLib
+import pathlib
 
 #Game constants
 #Window dimensions
@@ -9,9 +9,9 @@ SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Guizer Arcade"
 
 #Assets path
-ASSETS_PATH = pathlib.Path(__file).resolve().parent.parent / "assets"
+ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
 
-class Platformer(arcade.window):
+class Platformer(arcade.Window):
     def __init__(self) -> None:
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
@@ -46,3 +46,33 @@ class Platformer(arcade.window):
         self.victory_sound = aracade.load_sound(
             str(ASSETS_PATH / "sounds"/ "victory.wav")
         )
+
+    def setup(self):    #sets up the game to be begin playing. Add code that may need to be repeated throughout the game. eg initialize new levels on success or reset the current level on failure
+        """This function sets game for the current level"""
+        pass
+
+    def on_key_press(self, key: int, modifiers: int ):  ##on key press & release allows for processing keyboard input independently.      #
+        """Processes key presses
+
+        -key(int)---> which key was pressed
+        -modifiers(int)---> which modifiers where down at the time
+        """
+
+    def on_key_release(self, key: int, modifiers: int):
+        """Processes key releases"""
+
+    def on_update(self, delta_time: float):
+        """Updates the position of game objects
+
+        -delta_time(float)---> time after last call
+        """
+        pass
+
+    def on_draw(self):  #where everything displayed in your game drawn
+        pass
+
+
+if __name__ == "__main__": #main entry point of the game
+    window = Platformer()   #game object window based on the class defined
+    window.setup()  #setup the game by calling window.setup
+    arcade.run()    #start game loop by calling arcade.run                    
